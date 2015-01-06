43d9813482c8b7c766332432a94c84d1db9b1da0
swilso793 <swilso793@gmail.com>
2015-01-06 13:42:42 +0100

    up to 8 done

--- 005.py
+++ 005.py
@@ -9,4 +9,14 @@ What is the smallest number that is evenly divisible by all of the numbers
 from 1 to 20?
 """
 
+def find(n):
+    for i in range(20,0,-1):
+        if n%i!=0:
+            return
+    return 'done'
 
+for i in range (2500000,509005000,20):
+    if (find(i)):
+        print(i)
+        break
+        
---
+++ 006.py
@@ -0,0 +1,32 @@
+"""
+Project Euler Problem 6
+=======================
+
+The sum of the squares of the first ten natural numbers is,
+                       1^2 + 2^2 + ... + 10^2 = 385
+
+The square of the sum of the first ten natural numbers is,
+                    (1 + 2 + ... + 10)^2 = 55^2 = 3025
+
+Hence the difference between the sum of the squares of the first ten
+natural numbers and the square of the sum is 3025 - 385 = 2640.
+
+Find the difference between the sum of the squares of the first one
+hundred natural numbers and the square of the sum.
+"""
+
+
+def sum_of_squares(n=100):
+    if n == 0:
+        return 0 
+    return pow(n,2) + sum_of_squares(n-1)
+
+def sums(n=100):
+    if n == 0:
+        return 0
+    return n+sums(n-1)
+
+if __name__ == "__main__":
+    print(pow(sums(),2) - sum_of_squares())
+
+#print(sum_of_squares(10))
